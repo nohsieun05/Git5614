4fe27b34c608cd9c69654f07169c80200a7b6d68
nohsieun05 <nohsieun0428@gmail.com>
2026-04-26 12:24:03 +0900

    Add prime numbers in Testing branch

--- mynumber.c
+++ mynumber.c
@@ -2,16 +2,38 @@
 
 int main(void)
 {
-    int i;
+    int i, j, isPrime;
+
 
     for (i = 1; i <= 100; i += 2)
     {
         printf("%d\n", i);
     }
 
+
     for (i = 2; i <= 100; i += 2)
     {
         printf("%d\n", i);
+    }
+
+
+    for (i = 2; i <= 100; i++)
+    {
+        isPrime = 1;
+
+        for (j = 2; j < i; j++)
+        {
+            if (i % j == 0)
+            {
+                isPrime = 0;
+                break;
+            }
+        }
+
+        if (isPrime == 1)
+        {
+            printf("%d\n", i);
+        }
     }
 
     return 0;
