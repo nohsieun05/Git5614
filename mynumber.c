d2ef508446e4a606a2f8cabf6d951cc257fc1b5e
nohsieun05 <nohsieun0428@gmail.com>
2026-04-27 13:13:29 +0900

    Add mynumber program

--- mynumber.c
+++ mynumber.c
@@ -2,39 +2,9 @@
 
 int main(void)
 {
-    int i, j, isPrime;
-
-
-    for (i = 1; i <= 100; i += 2)
+    for (int i = 1; i <= 100; i += 2)
     {
         printf("%d\n", i);
     }
-
-
-    for (i = 2; i <= 100; i += 2)
-    {
-        printf("%d\n", i);
-    }
-
-
-    for (i = 2; i <= 100; i++)
-    {
-        isPrime = 1;
-
-        for (j = 2; j < i; j++)
-        {
-            if (i % j == 0)
-            {
-                isPrime = 0;
-                break;
-            }
-        }
-
-        if (isPrime == 1)
-        {
-            printf("%d\n", i);
-        }
-    }
-
     return 0;
 }
